1e6dcca34c49d17f31bc9477fd9dac167a1c6ce0
chuck francis <chuck_francis10@yahoo.com>
2020-04-13 22:20:51 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace REFandOUTINC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int iNum = 10;
+            Increment(iNum);
+            Console.Write("value of a is:" + iNum);
+            Console.Read();
+
+        }
+        public static void Increment(int b)
+        {
+            b = b + 10;
+        }
+    }
+}
